6d44713da5db890533ac4d093105527eddc645e3
Blair <mawubablair@gmail.com>
2025-12-01 17:32:38 +0300

    Add files via upload

---
+++ main (74).py
@@ -0,0 +1,11 @@
+try:
+    binary_file = open('file.bin', 'rb')
+    data = bytearray(binary_file.read(5))
+    binary_file.close()
+
+    for b in data:
+        print(hex(b), end=' ')
+
+except IOError as e:
+    print("I/O error occurred:", strerror(e.errno))
+    
